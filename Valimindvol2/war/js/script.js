4e43442f7f3e62700accaf0d1bf5ec50408e6f47
kyng <kyng386@hotmail.com>
2013-05-09 08:21:19 +0300

    Update script.js

--- Valimindvol2/war/js/script.js
+++ Valimindvol2/war/js/script.js
@@ -1,2 +1,15 @@
 $(document).ready(function(){
+  //$('#content').hide();
+	//$('#tervitus').show();
 });
+
+/*
+$("div.content:gt(0)").hide();  // to hide all div except for the first one
+$('#mainMenu li').click(function(selected) {
+   var getID = $(this).attr("class");      
+   var projectImages = $(this).attr("name");      
+
+   $("div.content").hide();       
+   $("#" + getID ).show();    
+});  
+*/
